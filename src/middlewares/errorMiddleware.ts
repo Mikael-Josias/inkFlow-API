9e821d53a0d56334055c83bdf851086d865cdd3f
Mikael Josias <mikaeljosias2002@gmail.com>
2023-06-06 10:11:41 -0300

    feat: error handling and request validation middlewares

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -2,6 +2,7 @@ import { IApplicationError } from '@/common/types'
 import { NextFunction, Request, Response } from 'express'
 
 export function ErrorMiddleware(err: IApplicationError | Error, req: Request, res: Response, next: NextFunction) {
+    if (err.name === 'BadRequestError') return res.status(400).send(err.message)
     if (err.name === 'ConflictError') return res.status(409).send(err.message)
     if (err.name === 'InternalError') return res.status(500).send(err.message)
 
